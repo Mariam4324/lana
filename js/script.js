4780013d4a87320d3c3c35c8806a52e29db173e6
Mariam4324 <ashkmariam@gmail.com>
2024-12-16 15:45:46 +0300

    sf

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
 const yearNode = document.getElementById("year");
 const currentYear = new Date().getFullYear();
+const classicCard = document.getElementById("classic");
+const selfCard = document.getElementById("self");
+const eriksonCard = document.getElementById("erikson");
+const skrCard = document.getElementById("skr");
+
+classicCard.addEventListener("click", function () {
+  window.location.href = "classic.html";
+});
+
+selfCard.addEventListener("click", function () {
+  window.location.href = "samogipnoz.html";
+});
+
+eriksonCard.addEventListener("click", function () {
+  window.location.href = "eriksonovsky.html";
+});
+
+skrCard.addEventListener("click", function () {
+  window.location.href = "skritni.html";
+});
 
 yearNode.innerText = currentYear;
