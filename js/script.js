28e34463c0544bae9857737afd6603082ce1b87b
Mariam4324 <ashkmariam@gmail.com>
2024-12-16 23:06:36 +0300

    fix: schedule added

--- js/script.js
+++ js/script.js
@@ -1,24 +1,26 @@
-const yearNode = document.getElementById("year");
-const currentYear = new Date().getFullYear();
-const classicCard = document.getElementById("classic");
-const selfCard = document.getElementById("self");
-const eriksonCard = document.getElementById("erikson");
-const skrCard = document.getElementById("skr");
+  // переход на страницы при нажатии на карточки курсов
+  const classicCard = document.getElementById("classic");
+  const selfCard = document.getElementById("self");
+  const eriksonCard = document.getElementById("erikson");
+  const skrCard = document.getElementById("skr");
 
-classicCard.addEventListener("click", function () {
-  window.location.href = "classic.html";
-});
+  classicCard.addEventListener("click", function () {
+    window.location.href = "classic.html";
+  });
 
-selfCard.addEventListener("click", function () {
-  window.location.href = "samogipnoz.html";
-});
+  selfCard.addEventListener("click", function () {
+    window.location.href = "samogipnoz.html";
+  });
 
-eriksonCard.addEventListener("click", function () {
-  window.location.href = "eriksonovsky.html";
-});
+  eriksonCard.addEventListener("click", function () {
+    window.location.href = "eriksonovsky.html";
+  });
 
-skrCard.addEventListener("click", function () {
-  window.location.href = "skritni.html";
-});
+  skrCard.addEventListener("click", function () {
+    window.location.href = "skritni.html";
+  });
 
-yearNode.innerText = currentYear;
+  // установка года в футере
+  const yearNode = document.getElementById("year");
+  const currentYear = new Date().getFullYear();
+  yearNode.innerHTML = currentYear;
